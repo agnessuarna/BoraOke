be1d0493d0c321df841b63646b3eee14a40ffd36
agnessuarna <agnessuarna@mail.ugm.ac.id>
2021-04-09 23:28:18 +0700

    Add project files.

---
+++ BoraOke/AespaAlbum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoraOke
+{
+    public class AespaAlbum : album
+    {
+        public override void Search()
+        {
+            Console.WriteLine("\nAlbum Black Mamba");
+            Console.WriteLine("Realese Date : November 17, 2020");
+
+        }
+    }
+}
+
---
+++ BoraOke/DataMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoraOke
+{
+    public struct DataMember
+    {
+        private string namaMember;
+        private int usiaMember;
+        private int IDMember;
+
+        public void input()
+        {
+            DataMember dataMember = new DataMember();
+
+            Console.Write("Nama \t\t : ");
+            dataMember.namaMember = Console.ReadLine();
+            Console.Write("Usia\t\t : ");
+            dataMember.usiaMember = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ID member \t : ");
+            dataMember.IDMember = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
